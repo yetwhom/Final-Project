1dfcc14ab9e610275e372ebbe4e1085fe58cb4e4
Yetwhom <128581038+Yetwhom@users.noreply.github.com>
2023-08-14 23:07:03 +0900

    test05

--- ima-nginx.sh
+++ ima-nginx.sh
@@ -2,6 +2,6 @@
 
 mv /home/ec2-user/jenkins/test/nginx/nginx/Dockerfile /home/ec2-user/jenkins/test/nginx/ 
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 028954022636.dkr.ecr.ap-northeast-2.amazonaws.com
-docker build -t test .
+docker build -t test /home/ec2-user/jenkins/test/nginx/Dockerfile
 docker tag test:latest 028954022636.dkr.ecr.ap-northeast-2.amazonaws.com/test:nginx1.0
 docker push 028954022636.dkr.ecr.ap-northeast-2.amazonaws.com/test:nginx1.0
