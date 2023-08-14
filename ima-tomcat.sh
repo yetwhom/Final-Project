8e02ebab92c448d28c73d29f236b178b798e81e3
Yetwhom <128581038+Yetwhom@users.noreply.github.com>
2023-08-14 23:00:48 +0900

    test 1

---
+++ ima-tomcat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mv /home/ec2-user/jenkins/test/tomcat/tomcat/Dockerfile /home/ec2-user/jenkins/test/tomcat/ 
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 028954022636.dkr.ecr.ap-northeast-2.amazonaws.com
+docker build -t test .
+docker tag test:latest 028954022636.dkr.ecr.ap-northeast-2.amazonaws.com/test:tomcat1.0
+docker push 028954022636.dkr.ecr.ap-northeast-2.amazonaws.com/test:tomcat1.0
